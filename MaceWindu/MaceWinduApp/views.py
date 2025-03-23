808990e3538bfd8dcc4ce2b8fd990f29910e460a
CRM-Thunder <damian-zamecki@wp.pl>
2025-03-23 18:04:10 +0100

    -created application "macewindu"
    -added mysql connection
    -modified requirements.txt with updated dependencies
    -added safe env storage for configuration (possible AWS Secret Manager implementation later)

---
+++ MaceWindu/MaceWinduApp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+# Create your views here.
