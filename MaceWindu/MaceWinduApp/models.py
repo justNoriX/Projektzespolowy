187221c66d5208a4dddc70468bf5f7d979b96892
CRM-Thunder <damian-zamecki@wp.pl>
2025-05-07 22:52:13 +0200

    modified User model to make username optional, email will be used as main authentication identifier

--- MaceWindu/MaceWinduApp/models.py
+++ MaceWindu/MaceWinduApp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
-    username = None
+    username = models.CharField(unique=True, max_length=50, null=True, blank=True)
     email = models.EmailField(
+        _('email address'),
         unique=True
     )
     first_name = models.CharField(
@@ -14,7 +15,7 @@ class User(AbstractUser):
         max_length=30
     )
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name','surname']
+    REQUIRED_FIELDS = ['username','first_name','surname']
 
     def __str__(self):
         return "{}".format(self.email)
